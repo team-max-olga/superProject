0c5b13b0b58ded9f702e30421c0d50a678b7a4cf
artweise <art2wsxweise7ujm@gmail.com>
2023-01-24 23:08:43 +0200

    added auth.routes for signup with valifation and made a form for signup.hbs

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,65 @@
-const express = require("express")
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+User = require("../models/User.model");
+const saltRounds = 10;
 
-const Max = Max
-/* trying to understand the branching */
+// GET route ==> to display the signup form to users
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
 
-const Olga = Olga
-/* still trying to understand */
+// POST route ==> to process form data
+router.post("/signup", (req, res, next) => {
+  //req.body what a user has submitted
+  console.log("Data from user: ", req.body);
+  const { username, email, password } = req.body;
+  // checking if all required fields are provided
+  if (!username || !email || !password) {
+    return res.render("auth/signup", {
+      errorMessage:
+        "All fields are mandatory. Please provide your username, email and password.",
+    });
+  }
+
+  // Validation of the username with regex
+  // Username may include _ and – having a length of 3 to 16 characters
+  const regexUsername = /^[a-z0-9_-]{3,16}$/;
+  if (!regexUsername.test(username)) {
+    return res.render("auth/signup", {
+      errorMessage:
+        "Please enter a username with a length of 3 to 16 characters. It may include '_'",
+    });
+  }
+
+  bcrypt
+    .hash(password, saltRounds)
+    // another approach
+    // at first generate the salt number, then to put salt number and bcrypted password together
+    /* bcrypt
+      .genSalt(saltRounds)
+      .then((salt) => {
+        return bcrypt.hash(password, salt);
+      }) */
+    .then((hashedPassword) => {
+      console.log("HashedPassword: ", hashedPassword);
+      return User.create({
+        username: username,
+        email: email,
+        // passwordHash => this is the key from the User model
+        // hashedPassword => this is placeholder (how we named returning value from the previous method .hash()
+        passwordHash: hashedPassword,
+      });
+    })
+    .then((newUserCreatedInDB) => {
+      //   console.log("New created user is: ", newUserCreatedInDB);
+      res.redirect("/login");
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
+});
+
+router.get("/login", (req, res, next) => res.render("auth/login.hbs"));
+
+module.exports = router;
