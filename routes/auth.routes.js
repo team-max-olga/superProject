1a1a42ff60406aa298f68ac4d17429a811815d63
artweise <art2wsxweise7ujm@gmail.com>
2023-01-27 12:40:41 +0200

    feat(login): added login routes, login.hbs, fixed styles
    login validation working only with username, needs to work with email!

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 User = require("../models/User.model");
 const saltRounds = 10;
+const mongoose = require("mongoose");
 
 // GET route ==> to display the signup form to users
 router.get("/signup", (req, res, next) => {
@@ -13,7 +14,7 @@ router.post("/signup", (req, res, next) => {
   //req.body what a user has submitted
   console.log("Data from user: ", req.body);
   const { username, email, password } = req.body;
-  // checking if all required fields are provided
+  // Checking if all required fields are provided
   if (!username || !email || !password) {
     return res.render("auth/signup", {
       errorMessage:
@@ -31,6 +32,18 @@ router.post("/signup", (req, res, next) => {
     });
   }
 
+  // Validation of the password with regex
+  // Password should have 1 lowercase letter, 1 uppercase letter, 1 number, and be at least 8 characters long
+
+  const regexPassword =
+    /(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/;
+  if (!regexPassword.test(password)) {
+    return res.render("auth/signup", {
+      errorMessage:
+        "Password should have 1 lowercase letter, 1 uppercase letter, 1 number, and be at least 8 characters long",
+    });
+  }
+
   bcrypt
     .hash(password, saltRounds)
     // another approach
@@ -41,7 +54,7 @@ router.post("/signup", (req, res, next) => {
         return bcrypt.hash(password, salt);
       }) */
     .then((hashedPassword) => {
-      console.log("HashedPassword: ", hashedPassword);
+      console.log("Hashed Password: ", hashedPassword);
       return User.create({
         username: username,
         email: email,
@@ -51,15 +64,86 @@ router.post("/signup", (req, res, next) => {
       });
     })
     .then((newUserCreatedInDB) => {
-      //   console.log("New created user is: ", newUserCreatedInDB);
+      console.log("New created user is: ", newUserCreatedInDB);
       res.redirect("/login");
     })
-    .catch((err) => {
-      console.log(err);
-      next(err);
+    .catch((error) => {
+      // Check if any of our mongoose validators are not being met
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render("auth/signup", { errorMessage: error.message });
+      }
+      // Check if the email is already registered with our website
+      else if (error.code === 11000 && error.keyPattern.email === 1) {
+        console.log(error);
+        res.render("auth/signup", {
+          errorMessage:
+            "There is already an account associated with this email please sign in or sign up with new email",
+        });
+      } else if (error.code === 11000 && error.keyPattern.username === 1) {
+        console.log(error);
+        res.render("auth/signup", {
+          errorMessage:
+            "This username already exists. Please choose another username.",
+        });
+      } else {
+        next(error);
+      }
     });
 });
 
 router.get("/login", (req, res, next) => res.render("auth/login"));
+
+router.post("/login", (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  // Checking if the user filled in all the required fields
+  if (!username || !email || !password) {
+    return res.render("auth/login", {
+      errorMessage:
+        "Login failed. Please enter a valid username, email and password",
+    });
+  }
+  // Checking if the user is already registered with our website
+  User.findOne({ username })
+    .then((user) => {
+      console.log("LOGED USER: ", user);
+      if (!user) {
+        res.render("auth/signup", {
+          errorMessage: "Username not found please sign up.",
+        });
+      }
+      //compareSync() is used to compare the user inputted password with the hashed password in the database
+      else if (bcrypt.compareSync(password, user.passwordHash)) {
+        res.render("profile/habitboard", user);
+      } else {
+        res.render("auth/login", { errorMessage: "Incorrect Password" });
+      }
+    })
+
+    //   // Checking if the email is already registered with our website
+    //   User.findOne({ email })
+    //     .then((userEmail) => {
+    //       console.log(userEmail);
+    //       if (!userEmail) {
+    //         res.render("auth/login", {
+    //           errorMessage:
+    //             "Email not found please sign up.",
+    //         });
+    //       }
+    //       else if (bcrypt.compareSync(password, user.passwordHash)) {
+    //         res.render("profile/habitboard", userEmail);
+    //       } else {
+    //         res.render("auth/login", { errorMessage: "Incorrect Password" });
+    //       }
+    //     })
+
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.get("/profile", (req, res, next) => {
+  res.render("profile/habitboard");
+});
 
 module.exports = router;
