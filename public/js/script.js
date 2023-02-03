acb866ffdaa6160ccb33fcf16752fdb6e60b8a05
Maksym Kopychanskyi <kopy.chan.max@gmail.com>
2023-02-03 20:43:50 +0000

    trying to implement modal window for the habit-details page

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,25 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("superProject JS imported successfully!");
 });
 
+const showButton = document.getElementById('showDialog');
+const favDialog = document.getElementById('favDialog');
+const outputBox = document.querySelector('output');
+const selectEl = favDialog.querySelector('select');
+const confirmBtn = favDialog.querySelector('#confirmBtn');
+
+// "Update details" button opens the <dialog> modally
+showButton.addEventListener('click', () => {
+    favDialog.showModal();
+});
+// "Favorite animal" input sets the value of the submit button
+selectEl.addEventListener('change', (e) => {
+  confirmBtn.value = selectEl.value;
+});
+// "Confirm" button of form triggers "close" on dialog because of [method="dialog"]
+favDialog.addEventListener('close', () => {
+  outputBox.value = `ReturnValue: ${favDialog.returnValue}.`;
+});
+
 /* function chooseDuration() {
   let days7 = document.getElementById("duration7days").checked
   let days14 = document.getElementById("duration14days").checked
