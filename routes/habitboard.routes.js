5814b7dd680cddf8acedb5ef349a1d6266b200b4
artweise <art2wsxweise7ujm@gmail.com>
2023-02-06 07:22:15 +0200

    feat(checkboxes): new approach with checkboxes implemented, the habitmodel changed, new routes for edit progression implemented
    fixed habit icons on profile page

--- routes/habitboard.routes.js
+++ routes/habitboard.routes.js
@@ -11,7 +11,6 @@ router.get("/profile", isLoggedIn, (req, res) => {
   User.findById(req.session.currentUser._id)
     .populate("habit")
     .then((info) => {
-      // console.log(inçfo)
       res.render("profile/habitboard", info);
     });
 });
@@ -19,93 +18,41 @@ router.get("/profile", isLoggedIn, (req, res) => {
 //route GET for create a new habit page (2)
 router.get("/profile/create-habit", (req, res) => {
   res.render("profile/create-habit");
-  // console.log("Create a new habit here");
 });
-
-let week1 = [
-  { name: "day-1", done: false },
-  { name: "day-2", done: false },
-  { name: "day-3", done: false },
-  { name: "day-4", done: false },
-  { name: "day-5", done: false },
-  { name: "day-6", done: false },
-  { name: "day-7", done: false },
-];
-let week2 = [
-  { name: "day-8", done: false },
-  { name: "day-9", done: false },
-  { name: "day-10", done: false },
-  { name: "day-11", done: false },
-  { name: "day-12", done: false },
-  { name: "day-13", done: false },
-  { name: "day-14", done: false },
-];
-let week3 = [
-  { name: "day-15", done: false },
-  { name: "day-16", done: false },
-  { name: "day-17", done: false },
-  { name: "day-18", done: false },
-  { name: "day-19", done: false },
-  { name: "day-20", done: false },
-  { name: "day-21", done: false },
-];
 
 //route POST for "create a new HABIT" (3)
 router.post("/profile/create-habit", (req, res) => {
-  const { title, category, duration, description, author } = req.body;
-  // console.log(req.body)
+  const { title, category, duration, description, author, days } = req.body;
+  console.log(req.body);
 
-  if ("days-7" in req.body) {
-    Habit.create({
-      week1Tracker: week1,
-      title,
-      category,
-      duration,
-      description,
-      author: req.session.currentUser._id,
-    }).then((habit) => {
+  let daysOfHabit = [];
+
+  if (days) {
+    console.log(days);
+    const arrayToLoop = Array.from(Array(Number(days)).keys());
+    console.log(arrayToLoop);
+    arrayToLoop.forEach((element) => {
+      const day = { name: `day${element + 1}`, done: false };
+      daysOfHabit = [...daysOfHabit, day];
+    });
+  }
+  console.log("daysOfHabit", daysOfHabit);
+  Habit.create({
+    title,
+    category,
+    duration,
+    description,
+    author: req.session.currentUser._id,
+    days: daysOfHabit,
+  })
+    .then((habit) => {
       return User.findByIdAndUpdate(req.session.currentUser._id, {
         $push: { habit: habit._id },
       });
+    })
+    .then(() => {
+      res.redirect("/profile");
     });
-  } else if ("days-14" in req.body) {
-    Habit.create({
-      week1Tracker: week1,
-      week2Tracker: week2,
-      title,
-      category,
-      duration,
-      description,
-      author: req.session.currentUser._id,
-    })
-      .then((habit) => {
-        return User.findByIdAndUpdate(req.session.currentUser._id, {
-          $push: { habit: habit._id },
-        });
-      })
-      .then(() => {
-        res.redirect("/profile");
-      });
-  } else if ("days-21" in req.body) {
-    Habit.create({
-      week1Tracker: week1,
-      week2Tracker: week2,
-      week3Tracker: week3,
-      title,
-      category,
-      duration,
-      description,
-      author: req.session.currentUser._id,
-    })
-      .then((habit) => {
-        return User.findByIdAndUpdate(req.session.currentUser._id, {
-          $push: { habit: habit._id },
-        });
-      })
-      .then(() => {
-        res.redirect("/profile");
-      });
-  }
 });
 
 //route GET for habit details page (4)
@@ -139,29 +86,7 @@ router.get("/profile/:habitId/edit", (req, res) => {
 //route POST for editing a habit (6)
 router.post("/profile/:habitId/edit", (req, res) => {
   const { habitId } = req.params;
-  const {
-    title,
-    category,
-    duration,
-    week1Tracker,
-    week2Tracker,
-    week3Tracker,
-    description,
-  } = req.body;
-  console.log("THIS IS IT", req.body);
-
-  Habit.findById(habitId).then((foundHabit) => {
-    Object.keys(req.body).forEach((key) => {
-      const test = foundHabit.week1Tracker.find(
-        (element) => element.name === key
-      );
-      // if(){
-
-      // }
-
-      console.log("TEST!!!!!!!!!!!", test);
-    });
-  });
+  const { title, category, duration, description } = req.body;
 
   Habit.findByIdAndUpdate(
     habitId,
@@ -169,14 +94,12 @@ router.post("/profile/:habitId/edit", (req, res) => {
     { new: true }
   )
     .then((updatedHabit) => {
-      console.log("Habit updated: " + updatedHabit);
       res.redirect(`/profile`);
     })
     .catch((error) => {
       console.log("Error occured while editing your habit: " + error);
     });
 });
-/* POST route not working */
 
 //route POST for deleting a habit (7)
 router.post("/profile/:habitId/delete", (req, res) => {
@@ -184,11 +107,38 @@ router.post("/profile/:habitId/delete", (req, res) => {
 
   Habit.findByIdAndDelete(habitId)
     .then((deletedHabit) => {
-      // console.log(deletedHabit + " was deleted.")
       res.redirect("/profile");
     })
     .catch((error) => {
       console.log("Error while deleting a habit: " + error);
+    });
+});
+
+//route POST for editing a progression (8)
+router.post("/profile/:habitId/edit-progression", (req, res) => {
+  const { habitId } = req.params;
+
+  const daysToChange = req.body;
+
+  Habit.findById(habitId)
+    .then((myHabit) => {
+      const newDays = myHabit.days.map((day) => {
+        const foundInDaysToChange = Object.keys(daysToChange).find(
+          (el) => el === day.name
+        );
+        if (foundInDaysToChange) {
+          day.done = true;
+        } else {
+          day.done = false;
+        }
+        return day;
+      });
+      return newDays;
+    })
+    .then((newDays) => {
+      Habit.updateOne({ _id: habitId }, { days: newDays }).then(() => {
+        res.redirect("/profile");
+      });
     });
 });
 
