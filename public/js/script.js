5ea767b645ba77691306e7d0808e55a4f2db52b1
artweise <art2wsxweise7ujm@gmail.com>
2023-02-07 20:10:38 +0200

    feat(styles): styling auth pages, navbar links, favicon

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,36 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("superProject JS imported successfully!");
 });
 
+// LOGIN form is working with Enter Key now
+const form = document.getElementById("login-form");
+const inputField = document.getElementById("input-field");
+const submitButton = document.getElementById("submit-button");
+
+inputField.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    submitButton.click();
+  }
+});
+
+// const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard.js");
+
+// LOGOUT button visible only for logged-in user
+// const isLoggedIn = true;
+
+// if (isLoggedIn) {
+//   document.getElementById("logout-button").style.display = "block";
+// }
+
+// function getUserStatus() {
+//   return fetch("/api/user/status")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (data.isLoggedIn) {
+//         document.getElementById("logout-button").style.display = "block";
+//       }
+//     })
+//     .catch((error) => console.error(error));
+// }
+
+// getUserStatus();
