14958cf48ff671f25c374cec8becda20e7c00e7e
Maksym Kopychanskyi <kopy.chan.max@gmail.com>
2023-02-07 18:47:05 +0000

    added popup structure

--- public/js/script.js
+++ public/js/script.js
@@ -36,3 +36,20 @@ inputField.addEventListener("keyup", function (event) {
 // }
 
 // getUserStatus();
+
+
+const showButton = document.getElementById('showDialog');
+const favDialog = document.getElementById('favDialog');
+const outputBox = document.querySelector('output');
+const selectEl = favDialog.querySelector('select');
+const confirmBtn = favDialog.querySelector('#confirmBtn');
+
+// "Update details" button opens the <dialog> modally
+showButton.addEventListener('click', () => {
+    favDialog.showModal();
+});
+// "Favorite animal" input sets the value of the submit button
+selectEl.addEventListener('change', (e) => {
+  confirmBtn.value = selectEl.value;
+});
+
