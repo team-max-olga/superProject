7ea8ddb201edf31db1dca54d36a499d0a116378b
artweise <art2wsxweise7ujm@gmail.com>
2023-02-10 23:45:23 +0200

    perf(profile): styles on all profile pages
    footer

--- routes/habitboard.routes.js
+++ routes/habitboard.routes.js
@@ -11,37 +11,46 @@ router.get("/profile", isLoggedIn, (req, res) => {
   User.findById(req.session.currentUser._id)
     .populate("habit")
     .then((info) => {
-      res.render("profile/habitboard", { info, theme: "colored" });
       // res.render("profile/habitboard", info);
+      res.render("profile/habitboard", { info, theme: "colored" });
     });
 });
 
 //route GET for create a new habit page (2)
 router.get("/profile/create-habit", (req, res) => {
-  res.render("profile/create-habit");
+  // res.render("profile/create-habit");
+  res.render("profile/create-habit", { theme: "colored" });
 });
 
 //route POST for "create a new HABIT" (3)
 router.post("/profile/create-habit", (req, res) => {
-  const { title, category, duration, description, author, days } = req.body;
-  console.log(req.body);
+  const { title, category, description, author, days } = req.body;
+  // console.log(days);
+
+  if (!title) {
+    // return res.render('profile/create-habit', {
+    return res.render("profile/create-habit", {
+      theme: "colored",
+      errorMessage: "The title field is mandatory. Please provide this detail.",
+    });
+  }
 
   let daysOfHabit = [];
 
   if (days) {
-    console.log(days);
+    // console.log(days);
     const arrayToLoop = Array.from(Array(Number(days)).keys());
-    console.log(arrayToLoop);
+    // console.log(arrayToLoop);
     arrayToLoop.forEach((element) => {
       const day = { name: `day${element + 1}`, done: false };
       daysOfHabit = [...daysOfHabit, day];
     });
   }
-  console.log("daysOfHabit", daysOfHabit);
+  // console.log("daysOfHabit", daysOfHabit);
   Habit.create({
     title,
     category,
-    duration,
+    duration: days,
     description,
     author: req.session.currentUser._id,
     days: daysOfHabit,
@@ -63,7 +72,8 @@ router.get("/profile/habit/:habitId", (req, res) => {
   Habit.findById(habitId)
     .then((habit) => {
       // console.log("This is your habit: " + habit);
-      res.render("profile/habit-details", habit);
+      // res.render("profile/habit-details", habit);
+      res.render("profile/habit-details", { habit, theme: "colored" });
     })
     .catch((error) => {
       console.log("There was an error retrieving your habit page: " + error);
@@ -76,8 +86,9 @@ router.get("/profile/:habitId/edit", (req, res) => {
 
   Habit.findById(habitId)
     .then((habitToEdit) => {
-      // console.log("The habit you want to edit is this one: " + habitToEdit)
-      res.render("profile/edit-habit", habitToEdit);
+      // console.log("The habit you want to edit is this one: " + habitToEdit);
+      // res.render("profile/edit-habit", habitToEdit);
+      res.render("profile/edit-habit", { habitToEdit, theme: "colored" });
     })
     .catch((error) => {
       console.log("there was an error trying to edit your habit: " + error);
